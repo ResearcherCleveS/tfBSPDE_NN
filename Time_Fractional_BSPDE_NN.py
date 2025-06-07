8096a3db8eaec31b039a8125890c77c550227c6e
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-07 19:00:52 -0400

    Update Time_Fractional_BSPDE_NN.py

--- Time_Fractional_BSPDE_NN.py
+++ Time_Fractional_BSPDE_NN.py
@@ -19,6 +19,6 @@ if st.button("**Summary**"):
 with st.sidebar:
   st.header("**Caputo Integer-Valued Derivatives**")
   st.write("May need to convert to integer value representations of fraction values.")
-  st.slider(st.markdown("$\textsf{d\over{dx}}$"), min_value=0.0, max_value=1.0, step=0.125, format='%.3f')
+  st.slider(st.markdown("$\sf d\over{\sf dx}$"), min_value=0.0, max_value=1.0, step=0.125, format='%.3f')
 
 
