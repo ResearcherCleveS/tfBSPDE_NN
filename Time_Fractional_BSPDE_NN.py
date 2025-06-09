a609b5e0fc6f76d93c8154b114377b521221835f
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-09 11:32:24 -0400

    Update Time_Fractional_BSPDE_NN.py

--- Time_Fractional_BSPDE_NN.py
+++ Time_Fractional_BSPDE_NN.py
@@ -14,8 +14,7 @@ import matplotlib.pyplot as plt
 
 st.title("Time Fractinal Black-Scholes Partial Differential Equation")
 
-if st.sidebar:
-  st.button("**Summary**")
+with st.sidebar.button("**Summary**")
   st.subheader("**AI plus Partial Differential Eqns ie Physics Informed Neural Network (PINN) demonstration.**")
   st.write("**We'll explore implementing a PINN to optimize the efficiency and application of the Black Scholes Formula.**")
 
