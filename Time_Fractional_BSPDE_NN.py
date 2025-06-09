79b92029c4103ae25e9cb710a54b426e2ad95819
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-09 11:53:49 -0400

    Update Time_Fractional_BSPDE_NN.py

--- Time_Fractional_BSPDE_NN.py
+++ Time_Fractional_BSPDE_NN.py
@@ -29,8 +29,8 @@ st.sidebar.header("**Caputo Integer-Valued Derivatives**")
 st.sidebar.write("May need to convert to integer value representations of fraction values.")
 st.sidebar.slider("𝗗𝛂", min_value=0.0, max_value=1.0, step=0.125, format='%.3f')
 
-st.sidebar.header("**𝗗𝛂**")
-Caputa Fractional Derivative = st.sidebar.number_input(
+st.sidebar.header("**Caputa Fractional Derivative**")
+𝗗𝛂 = st.sidebar.number_input(
     "𝗗𝛂",
     min_value=0.0,
     max_value=1.5,
